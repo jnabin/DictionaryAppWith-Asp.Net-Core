0d79872a65789f99a5178faa824dd8700a8bf481
jnabin <jahangirnabin2@gmail.com>
2021-07-06 18:53:22 +0600

    small bug fix2

--- DictionaryApp/wwwroot/js/site.js
+++ DictionaryApp/wwwroot/js/site.js
@@ -2,3 +2,14 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+$('#searchWord').mouseenter(function () {
+    $('#livesearch').stop().show();
+});
+$('input[type=search]').on('input', function () {
+    $('#livesearch').stop().show();
+});
+$("#searchWord").mouseleave(function () {
+    if (!$('#livesearch').is(':hover')) {
+        $('#livesearch').hide();
+    };
+});
